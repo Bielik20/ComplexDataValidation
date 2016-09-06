e37564179b3552a216b0507ce561c426a943742e
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-09-06 11:42:03 +0200

    Added Models

---
+++ src/ComplexDataValidation/Models/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComplexDataValidation.Models
+{
+    public class Child
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int ParentID { get; set; }
+        public Parent Parent { get; set; }
+    }
+}
