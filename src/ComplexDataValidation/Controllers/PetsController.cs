bb8cb7a416d644607f73916c988ff9b241139ec0
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-09-07 15:19:20 +0200

    Pet Scaffolded

---
+++ src/ComplexDataValidation/Controllers/PetsController.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ComplexDataValidation.Data;
+using ComplexDataValidation.Models;
+
+namespace ComplexDataValidation.Controllers
+{
+    public class PetsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PetsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Pets
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pets.Include(p => p.Person);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.SingleOrDefaultAsync(m => m.ID == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pets/Create
+        public IActionResult Create()
+        {
+            ViewData["PersonID"] = new SelectList(_context.People, "ID", "ID");
+            return View();
+        }
+
+        // POST: Pets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Kind,Name,PersonID,Submited")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["PersonID"] = new SelectList(_context.People, "ID", "ID", pet.PersonID);
+            return View(pet);
+        }
+
+        // GET: Pets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.SingleOrDefaultAsync(m => m.ID == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            ViewData["PersonID"] = new SelectList(_context.People, "ID", "ID", pet.PersonID);
+            return View(pet);
+        }
+
+        // POST: Pets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Kind,Name,PersonID,Submited")] Pet pet)
+        {
+            if (id != pet.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PetExists(pet.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["PersonID"] = new SelectList(_context.People, "ID", "ID", pet.PersonID);
+            return View(pet);
+        }
+
+        // GET: Pets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pet = await _context.Pets.SingleOrDefaultAsync(m => m.ID == id);
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pet = await _context.Pets.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Pets.Remove(pet);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool PetExists(int id)
+        {
+            return _context.Pets.Any(e => e.ID == id);
+        }
+    }
+}
