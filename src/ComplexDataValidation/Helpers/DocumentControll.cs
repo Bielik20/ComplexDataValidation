b80c07e283a29bbd37845a9507cff2fe20d4313d
Damian Bielecki <db460597@wmservice.corpnet1.com>
2016-09-22 13:57:50 +0200

    Added validation methods to DC

--- src/ComplexDataValidation/Helpers/DocumentControll.cs
+++ src/ComplexDataValidation/Helpers/DocumentControll.cs
@@ -1,5 +1,6 @@
 ﻿using ComplexDataValidation.Data;
 using ComplexDataValidation.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +19,10 @@ namespace ComplexDataValidation.Helpers
             _entManager = entManager;
         }
 
-        public bool BookFilled(Book book)
+        /// <summary>
+        /// Checks if book has all information filled.
+        /// </summary>
+        public bool BookIsFilled(Book book)
         {
             if (book.Information == null)
             {
@@ -26,6 +30,71 @@ namespace ComplexDataValidation.Helpers
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Checks if Book is last in specific person list.
+        /// </summary>
+        public async Task<bool> BookIsLast(string bookId, string personId)
+        {
+            var booksQuery = _context.Books
+                       .Where(p => p.PersonId == personId)
+                       .Select(p => p);
+            var books = await booksQuery.ToListAsync();
+
+            foreach (var book in books)
+            {
+                book.Information = await _context.Information.SingleOrDefaultAsync(x => x.Id == book.Id);
+            }
+
+            books.Sort();
+
+            if (bookId == books.Last().Id)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if there are any chapters in that book.
+        /// </summary>
+        public async Task<bool> BookIsEmpty(Book book)
+        {
+            var chaptersQuery = from c in _context.Chapters
+                                where c.BookId == book.Id
+                                orderby c.CreationDate
+                                select c;
+            if (await chaptersQuery.AnyAsync())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if date is youngest in specific person considering books and chapters.
+        /// </summary>
+        public async Task<bool> DateIsYoungest(DateTime date, string personId)
+        {
+            var booksQuery = _context.Books
+                      .Where(p => p.PersonId == personId)
+                      .Select(p => p);
+            var books = await booksQuery.ToListAsync();
+
+            foreach (var book in books)
+            {
+                book.Information = await _context.Information.SingleOrDefaultAsync(x => x.Id == book.Id);
+            }
+            books.Sort();
+            var lastBook = books.Last();
+            await _entManager.RetrieveChapters(lastBook.Chapters, lastBook);
+
+            if (lastBook.Chapters == null)
+            {
+                return date < lastBook.Information.CreationDate;
+            }
+            return date < lastBook.Chapters.Last().CreationDate;
         }
     }
 }
